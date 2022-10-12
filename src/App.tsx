57587d659c9879c287da568400703ca6bded72d3
huasmc <huasmc@gmail.com>
2022-10-12 15:14:29 -0400

    style: mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -20,27 +20,29 @@ function App() {
       <a href="https://github.com/huasmc/tic-tac-toe-react-ts" target="_blank">
         <img id="gh-logo" alt="" src="github.png" />
       </a>
-      <div className="content">
-        <Row>
-          <Col xs={{ span: 3, order: 3 }} sm={{ span: 3, order: 1 }}>
-            <Feed />
-          </Col>
-          <Col
-            xs={{ span: 7, order: 2 }}
-            sm={{ span: 7, order: 2 }}
-            className="board"
-          >
-            <TokenSelector
-              playerToken={playerToken}
-              setPlayerToken={setPlayerToken}
-              resetToken={resetToken}
-            />
+      <Row>
+        <Col
+          className="d-none d-lg-block"
+          xs={{ span: 3, order: 3 }}
+          sm={{ span: 3, order: 1 }}
+        >
+          <Feed />
+        </Col>
+        <Col
+          xs={{ span: 7, order: 2 }}
+          sm={{ span: 7, order: 2 }}
+          className="board"
+        >
+          <TokenSelector
+            playerToken={playerToken}
+            setPlayerToken={setPlayerToken}
+            resetToken={resetToken}
+          />
 
-            <Board playerToken={playerToken} setThinking={setThinking} />
-          </Col>
-          <Col xs={{ span: 4, order: 1 }} sm={{ span: 4, order: 3 }} />
-        </Row>
-      </div>
+          <Board playerToken={playerToken} setThinking={setThinking} />
+        </Col>
+        <Col xs={{ span: 4, order: 1 }} sm={{ span: 4, order: 3 }} />
+      </Row>
       <Row>
         <div
           style={{
